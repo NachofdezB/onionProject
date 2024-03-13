128e54363e0348ea72e5a8d234e3c4762b44b761
Nacho <ignacio.fernandez12@educa.madrid.org>
2024-03-13 12:28:54 +0100

    Commit inicial resta

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    /**
+     * Método resta
+     * @param a El primer número a sumar.
+     * @param b El segundo número a sumar.
+     * @return La resta de los dos números.
+     */
+    public double restar(double a, double b) {
+        return (a - b);
+    }
+}
