8601fe10818716f98b79828365bfa7056e617c79
Nacho <ignacio.fernandez12@educa.madrid.org>
2024-03-13 12:28:17 +0100

    Commit inicial suma

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    /**
+     * Método suma
+     * @param a El primer número a sumar.
+     * @param b El segundo número a sumar.
+     * @return La suma de los dos números.
+     */
+    public double sumar(double a, double b) {
+        return (a + b);
+    }
+}
