c7fcf703c8dc6157e5e6ae6af7b0c477c60b01cd
Nacho <ignacio.fernandez12@educa.madrid.org>
2024-03-13 12:29:59 +0100

    Commit inicial multiplicar

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    /**
+     * Método multiplicar
+     * @param a El primer número a sumar.
+     * @param b El segundo número a sumar.
+     * @return La multiplicacion de los dos números.
+     */
+    public double multiplicar(double a, double b) {
+        return (a * b);
+    }
+}
